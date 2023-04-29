c1ee733000af1f3b6f01f35464c4dd59f003bbbc
jisooou <87888800+jisooou@users.noreply.github.com>
2023-04-29 16:41:08 +0900

    Upload basic.js file
    basic.js 연습 파일

---
+++ basic.js
@@ -0,0 +1,21 @@
+//let은 변경될 수 있는 값
+//const는 변경될 수 없는 값. 수정하면 오류가 난다. 
+
+//문자형 String 
+const name = "Mike"; 
+const age = 30; 
+
+const name1 = "Mike"
+const name2 = 'Mike'
+const name3 = `Mike`
+
+const message = "I'm a boy.";
+const message2 = 'I\'m a boy.';
+
+const message3 = `My name is ${name}`;
+
+const message4 = `나는 ${30+1}살 입니다.`;
+console.log(message4)
+
+
+
