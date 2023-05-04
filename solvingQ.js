e1cd4621998aca44a275f3146b694f1c3bfc4383
jisooou <87888800+jisooou@users.noreply.github.com>
2023-05-04 21:03:40 +0900

    Update solvingQ.js
    05/04 TIL - solved 05/03 TIL

--- solvingQ.js
+++ solvingQ.js
@@ -56,17 +56,18 @@ printSquare(6);
 
 
 /*내 노트북 용량은?*/
-function teraToGiga() {
-  console.log(teraToGiga + '는')
-  console.log(teraToGiga * 2 + 'GB 입니다.')
-};
+function teraToGiga(volume) {
+  console.log(volume + 'TB는');
+  console.log(volume * 1024 + 'GB 입니다.');
+}
+function teraToMega(volume) {
+  console.log(volume + 'TB는');
+  console.log(volume * 1024 * 1024 + 'MB 입니다.');
+}
 
-function teraToMega() {
-  console.log(teraToMega + '는')
-  console.log(teraToMega )
-};
 // TB -> GB 테스트
 teraToGiga(2);
 // TB -> MB 테스트
 teraToMega(2);
+
 
