3171d9e7883826b8a951988f999255c21affb184
jisooou <87888800+jisooou@users.noreply.github.com>
2023-06-09 02:42:30 +0900

    Update func.js
    06/09 수정

--- func.js
+++ func.js
@@ -1,6 +1,6 @@
-// main.js
 const img1 = document.getElementById("image1");
 const img2 = document.getElementById("image2");
+
 
 text ="";
 var images=[]; // 원래 넣는 배열
@@ -9,6 +9,17 @@ var sIndices=[];
 var gameRound = 16;
 var num=0;
 var cnt=0;
+
+
+document.getElementById("resetButton").addEventListener("click", function() {
+    var destination = "gender.html";
+    window.location.href = destination;
+});
+document.getElementById("plusButton").addEventListener("click", function() {
+    var destination = "plus.html";
+    window.location.href = destination;
+});
+
 
 function zoomInImg1(event) {
     img1.style.width = "600px";
@@ -72,44 +83,17 @@ function fadeOut2(event) {
     }, 50);
 }
 
-function centerImage(img, index, total) {
-    var containerWidth = img.parentElement.offsetWidth;
-    var imageWidth = img.offsetWidth;
-    var offset = 50; // Adjust the offset value as needed
-    var marginLeft = (containerWidth / 2) - ((total * imageWidth) / 2) + (index * imageWidth) + offset;
-
-    img.style.position = "absolute";
-    img.style.left = marginLeft + "px";
-    img.style.transform = "translateX(-50%)";
-    img.style.margin = "auto"; // Add this line to center the image horizontally
-}
-
-
-
-// 이미지를 이동시키는 도우미 함수
-function slideImageLeft(img) {
-    img.style.position = "fixed";
-    img.style.transform = "translate(-200%, 0)";
-    img.style.behavior = "smooth";
-}
-
-function slideImageRight(img) {
-    img.style.position = "fixed";
-    img.style.transform = "translate(100%, 0)";
-    img.style.behavior = "smooth";
-}
-
 function shuffle()
 { // 배열을 섞는다.
     if (gameRound >= 4) {
-         // 배열을 랜덤으로 섞음
-         indices.sort(function(a,b){return 0.5- Math.random()});
+        // 배열을 랜덤으로 섞음
+        indices.sort(function(a,b){return 0.5- Math.random()});
 
-         // 몇 강?
-         text = gameRound + "강!";
+        // 몇 강?
+        text = gameRound + "강!";
     } else if (gameRound >= 2){
         text = "결승!!!";
-		indices.sort(function(a,b){return 0.5- Math.random()});
+    indices.sort(function(a,b){return 0.5- Math.random()});
     } else {
         text = "최종 우승!!!";
     }
@@ -119,26 +103,23 @@ function shuffle()
     document.getElementById('cal').innerHTML=text;
 }
 
-function showImg(num){ // 사진을 보여줌.
-    if(images.length == 1){
-        img2.style.display='none';
-      } else {
-        img2.src=images[num+1];
-        // id images를 배열의 +1로 대체
+
+
+
+function showImg(num) {
+    if (images.length == 1) {
+        img2.style.display = 'none';
+        img1.style.display = 'block'; 
+    } else {
+        img2.style.display = 'block';
+        img2.src = images[num + 1];
     }
-    img1.src=images[num];
-    // id image를 배열로 대체
+    img1.src = images[num];
 }
 
-   function init() {
-      for(i=0; i < gameRound; i++) {
-         indices[i] = i + 1;
-      }
-      shuffle();
-      showImg(0);
-   }
 
-function change(n){
+
+function change(n) {
     sIndices[num] = indices[cnt + n];
     num++;
     cnt += 2;
@@ -152,14 +133,21 @@ function change(n){
         gameRound /= 2;
         shuffle();
         showImg(0);
+        if (gameRound < 2) {
+            var resetButton = document.getElementById("resetButton");
+            var plusButton = document.getElementById("plusButton");
+            resetButton.style.display = "inline-block"; // 최종 우승 페이지에서 버튼을 보이도록 설정
+            plusButton.style.display = "inline-block"; // 최종 우승 페이지에서 버튼을 보이도록 설정
+            clearInterval(timer);
+            progressBar.style.width = "0%";
+        }
     } else {
         showImg(cnt);
     }
 }
 
+
 init();
-
-
 // img1에 대한 줌 이벤트
 img1.addEventListener("mouseenter", zoomInImg1);
 img1.addEventListener("mouseleave", zoomOutImg1);
@@ -168,28 +156,64 @@ img1.addEventListener("mouseleave", zoomOutImg1);
 img2.addEventListener("mouseenter", zoomInImg2);
 img2.addEventListener("mouseleave", zoomOutImg2);
 
-// 클릭 이벤트
-img1.addEventListener("click", function (event) {
-    slideImageRight(img2);
-    fadeOut2(event);
-    
-    setTimeout(function() {
-        fadeIn(img2);
-        centerImage(img1,0,2);
-        centerImage(img2,1,2);
-        change(0);
-    }, 2000);
-    
-});
-img2.addEventListener("click", function (event) {
-    slideImageLeft(img1);
-    fadeOut1(event);
+// timer
+var timer;
+var progressBar;
+var remainingTime = 30; // Initial timer value
 
-    setTimeout(function() {
-        fadeIn(img1);
-        centerImage(img1,0,2);
-        centerImage(img2,1,2);
-        change(1);
-    }, 2000);
-    
+function startTimer() {
+progressBar.style.width = "100%"; // Initialize the progress bar
+remainingTime = 6; // Set the initial timer value
+timer = setInterval(function() {
+    if (remainingTime > 0) {
+    remainingTime--; // Decrease remaining time by 1 second
+    var progressWidth = (remainingTime / 6) * 100; // Calculate the width of the progress bar
+    progressBar.style.width = progressWidth + "%";
+    } else {
+    clearInterval(timer);
+    change(0); // Switch to the next picture when the timer reaches 0
+    restartTimer(); // Restart the timer
+    }
+}, 1000); // Update the timer every 1 second
+}
+
+function restartTimer() {
+clearInterval(timer);
+startTimer();
+}
+
+// 타이머에 대한 클릭 이벤트
+window.addEventListener("load", function() {
+progressBar = document.createElement("div");
+progressBar.classList.add("timer-bar");
+progressBar.innerHTML = '<div class="timer-progress"></div>';
+document.body.appendChild(progressBar);
+startTimer();
+});
+
+// 클릭 이벤트
+img1.addEventListener("click", function(event) {
+//slideImageRight(img2);
+fadeOut2(event);
+
+setTimeout(function() {
+    fadeIn(img2);
+    //centerImage(img1, 0, 2);
+    //centerImage(img2, 1, 2);
+    restartTimer(); // Restart the timer when clicking on a picture
+    change(0);
+}, 2000);
+});
+
+img2.addEventListener("click", function(event) {
+//slideImageLeft(img1);
+fadeOut1(event);
+
+setTimeout(function() {
+    fadeIn(img1);
+    //centerImage(img1, 0, 2);
+    //centerImage(img2, 1, 2);
+    restartTimer(); // Restart the timer when clicking on a picture
+    change(1);
+}, 2000);
 });
